83ccd9d714812ad023d16d27aa6e398a611591d2
damoski2 <62991627+damoski2@users.noreply.github.com>
2020-12-08 10:28:17 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[800]),
+        home: RandomWords());
+  }
+}
